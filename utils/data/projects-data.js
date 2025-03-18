99b0568c957c2f6a548a202730d0c458171d1139
Yogesh Patidar <yogeshpatidar340@gmail.com>
2025-03-18 12:00:30 +0530

    add project

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -3,6 +3,16 @@
 export const projectsData = [
     {
         id: 1,
+        name: 'Sens AI',
+        description: "Sens AI is an advanced AI-powered tool designed to craft professional resumes, cover letters, and assist with interview preparation.Built with cutting - edge technologies, it features a sleek design using ShadCN, efficient data handling with Neon Database(PostgreSQL) and Prisma ORM, and seamless workflow automation with Inngest.Powered by Gemini AI, Sens AI ensures top - notch career support with intelligent and personalized insights.",
+        tools: [, 'PostgressDB', 'Next.js', 'Shadecn', 'Inngest', 'TailwindCss', 'NeonDB', 'Prisma ORM'],
+        role: 'Full Stack Developer',
+        code: 'https://github.com/Yogeshpatidar1122/Sensai',
+        demo: 'https://sensai-nu-lilac.vercel.app/',
+        image: '/image/grocerystore.png',
+    },
+    {
+        id: 2,
         name: 'Grocery Store',
         description: "This project is a modern web application designed for managing a grocery store, built using the following technologies Strapi: A powerful and flexible headless CMS that provides an easy way to manage content and APIs.ShadCN: A component library that brings accessibility and design consistency to the front-end.Next.js: A React framework that offers server-side rendering, static site generation, and a seamless development experience.MongoDB: A NoSQL database used for efficient data storage and retrieval.The combination of these tools allows for a dynamic and scalable grocery store platform, enabling easy content management, efficient user interactions, and robust backend support.",
         tools: ['Express', 'MongoDB', 'Next.js', 'React.js', 'Strapi', 'TailwindCss'],
@@ -12,7 +22,7 @@ export const projectsData = [
         image: '/image/grocerystore.png',
     },
     {
-        id: 2,
+        id: 3,
         name: ' ChatApp',
         description: "This project is a modern web application designed for managing a grocery store, built using the following technologies Strapi: A powerful and flexible headless CMS that provides an easy way to manage content and APIs.ShadCN: A component library that brings accessibility and design consistency to the front-end.Next.js: A React framework that offers server-side rendering, static site generation, and a seamless development experience.MongoDB: A NoSQL database used for efficient data storage and retrieval.The combination of these tools allows for a dynamic and scalable grocery store platform, enabling easy content management, efficient user interactions, and robust backend support.",
         tools: ['NextJS', 'Tailwind CSS', "TypeScript", "Strapi CMS", "MongoDB", "Firebase"],
@@ -22,7 +32,7 @@ export const projectsData = [
         image: '/image/chatapp.png',
     },
     {
-        id: 3,
+        id: 4,
         name: 'Portfolio Website',
         description: "Hello and thank you for visiting my personal portfolio.Here, you'll find a curated collection showcasing my skills, experiences, and passion in various domains. As a versatile individual with a commitment to excellence, I've crafted this portfolio to offer you insight into my journey, projects, and contributions.",
 
@@ -33,7 +43,7 @@ export const projectsData = [
         image: '',
     },
     {
-        id: 4,
+        id: 5,
         name: 'AmazonPro||E-commerce Application',
         description: "AmazonPro is an Ecommerce website clone to purchase the things online.Crafted user-friendly interfaces with a keen emphasis on intuitive navigation, delivering a seamless shopping experience that delights customers., Created secure stripe payment gateway, ensuring transaction safety and reliability for user during checkout process Optimized backend systems, utilizing technologies such as, React, MongoDB, Stripe, Tailwind Css, Mongoose, JWT for authentication and Tailwind to enhance platform performance ",
         tools: ['NextJS', 'Material UI', "MongoDB", "Prisma", "Tailwind CSS",],
